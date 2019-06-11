5fe7f28a53dcee232ce684e84593fb67a8f6c2e7
offcn-java <myphone@163.com>
2019-06-11 16:29:28 +0800

    ceshi

---
+++ src/main/webapp/js/controller/itemCatController.js
@@ -0,0 +1,55 @@
+//item_cat控制层 
+app.controller('itemCatController' ,function($scope, $controller, itemCatService){	
+	
+	// 继承
+	$controller("baseController", {
+		$scope : $scope
+	});
+	
+	// 保存
+	$scope.save = function() {
+		itemCatService.save($scope.entity).success(function(response) {
+			if (response.success) {
+				// 重新加载
+				$scope.reloadList();
+			} else {
+				alert(response.message);
+			}
+		});
+	}
+	
+	//查询实体 
+	$scope.findOne = function(id){				
+		itemCatService.findOne(id).success(
+			function(response){
+				$scope.entity= response;					
+			}
+		);				
+	}
+	
+	//批量删除 
+	$scope.dele = function(){			
+		//获取选中的复选框			
+		itemCatService.dele($scope.selectIds).success(
+			function(response){
+				if(response.success){
+					$scope.reloadList();
+					$scope.selectIds=[];
+				}						
+			}		
+		);				
+	}
+	
+	// 定义搜索对象 
+	$scope.searchEntity = {};
+	// 搜索
+	$scope.search = function(page,size){			
+		itemCatService.search(page,size,$scope.searchEntity).success(
+			function(response){
+				$scope.list=response.rows;	
+				$scope.paginationConf.totalItems=response.total;
+			}			
+		);
+	}
+    
+});	
